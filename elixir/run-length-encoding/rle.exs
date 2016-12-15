62a7c17ae4b77f75b916ab5adcdefb642bb441a2
Dorian Karter <dkarter@gmail.com>
2016-12-14 19:30:16 -0600

    RunLengthEncoding exercism

---
+++ elixir/run-length-encoding/rle.exs
@@ -0,0 +1,35 @@
+defmodule RunLengthEncoder do
+  @spec encode(String.t) :: String.t
+  def encode(string) do
+    chars =
+      string
+      |> String.split("", trim: true)
+    do_encode(chars, Enum.at(chars, 0), 0, "")
+  end
+
+  defp do_encode([], nil, 0, ""), do: ""
+
+  defp do_encode([], last, count, str) do
+    str <> Integer.to_string(count) <> last
+  end
+
+  defp do_encode([current|t], last, count, str) when current == last do
+    do_encode(t, current, count + 1, str)
+  end
+
+  defp do_encode([current|t], last, count, str) when current != last do
+    str = str <> Integer.to_string(count) <> last
+    do_encode(t, current, 1,  str)
+  end
+
+  @spec decode(String.t) :: String.t
+  def decode(string) do
+    Regex.scan(~r/(?:(\d+)(\w))/, string, capture: :all_but_first)
+    |> Enum.map(&decode_pair/1)
+    |> Enum.join("")
+  end
+
+  defp decode_pair([count, letter]) do
+    String.duplicate(letter, String.to_integer(count))
+  end
+end
